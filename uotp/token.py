820f739bfce56072f62f751d93d5e3924cb75575
Bae Junehyeon <devunt@gmail.com>
2017-08-30 13:56:06 +0900

    Clean up token generating code

--- uotp/token.py
+++ uotp/token.py
@@ -5,50 +5,50 @@ from .util import Util
 
 
 class OTPTokenGenerator:
-    def __init__(self, oid, seed):
+    def __init__(self, oid: int, seed: bytes):
         self.__oid = oid
         self.__seed = seed
         self.__timediff = 0
 
-    def compensate_time_deviation(self, timediff):
+    def compensate_time_deviation(self, timediff: int):
         self.__timediff = timediff
 
-    def generate_token(self):
+    def generate_token(self) -> str:
         now = Util.now()
         now += self.__timediff
 
         time = now // 10
         oid = self.__oid
 
-        seed_oid = bytearray(11)
-        seed_time = bytearray(11)
+        acc_seed = bytearray(11)
+        time_seed = bytearray(11)
 
-        for i in reversed(range(0, 11)):
-            seed_time[i] = int(time) & 255
-            seed_oid[i] = int(oid) & 255
+        for i in reversed(range(11)):
+            acc_seed[i] = int(oid) & 255
+            time_seed[i] = int(time) & 255
 
             oid >>= 8
             time >>= 8
 
-        seed_oid += self.__seed
+        acc_seed += self.__seed
 
-        seed = bytearray(64)
-        seed[:20] = sha1(seed_oid).digest()
+        acc_seed_0 = bytearray(64)
+        acc_seed_0[:20] = sha1(acc_seed).digest()
 
-        seed_1 = seed[:]
-        seed_2 = seed[:]
+        acc_seed_1 = acc_seed_0[:]
+        acc_seed_2 = acc_seed_0[:]
 
-        for i in range(0, 64):
-            seed_1[i] ^= 54
-            seed_2[i] ^= 92
+        for i in range(64):
+            acc_seed_1[i] ^= 54
+            acc_seed_2[i] ^= 92
 
-        digest = sha1(seed_1 + seed_time).digest()
-        digest = sha1(seed_2 + digest).digest()
+        digest = sha1(acc_seed_1 + time_seed).digest()
+        digest = sha1(acc_seed_2 + digest).digest()
 
         digit = digest[-1] & 0xf
 
         # noinspection PyShadowingNames
-        token = reduce(lambda r, i: (r << 8) | digest[digit + i], range(0, 4))
+        token = reduce(lambda r, i: (r << 8) | digest[digit + i], range(4))
         token &= 0xffffdb
 
         rem = now % 30
